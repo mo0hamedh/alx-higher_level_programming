0f38922991753858f93f7cf94d1af94af31c61a0
mo0hamedh <mohamedhosamopp122@icloud.com>
2024-02-02 12:58:23 -0800

    hi

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Square class """
-class Square :
-    """class attributes"""
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square. No body."""
     pass
